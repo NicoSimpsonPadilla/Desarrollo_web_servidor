fe4461983d6e6d56451422be559ae4f65a677e28
NicoSimpsonPadilla <nicosimpsonpadilla@gmail.com>
2023-10-23 13:31:26 +0200

    update

--- Validacion_formulario/datos_formulario.php
+++ Validacion_formulario/datos_formulario.php
@@ -31,16 +31,31 @@
     }
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $temp_usuario = depurar($_POST["usuario"]);
+
         $temp_nombre = depurar($_POST["nombre"]);
         // Para que los espacios en blanco no cuenten, para cod interno
-        $temp_nombre = preg_replace("/^[ ]{2,}/", ' ', $temp_nombre);
+        $temp_nombre = preg_replace("/[ ]{2,}/", ' ', $temp_nombre);
 
         $temp_apellidos = depurar($_POST["apellidos"]);
         // Para que los espacios en blanco no cuenten, para cod interno
-        $temp_apellidos = preg_replace("/^[ ]{2,}/", ' ', $temp_apellidos);
+        $temp_apellidos = preg_replace("/[ ]{2,}/", ' ', $temp_apellidos);
 
         $temp_nacimiento = depurar($_POST["nacimiento"]);
     
+        if(!strlen($temp_usuario) > 0) {
+            $err_usuario = "El nombre de usuario es obligatorio";
+        } else {
+            // /^[a-zA-Z0-9]{4,8}$/
+            $patron = "/^[a-zA-Z0-9]{4,8}$/";
+            if(!preg_match($patron, $temp_usuario)) {
+                $err_usuario = "El nombre debe tener entre 4 y 8 caracters
+                y contener solamente letras o números";
+            } else {
+                $usuario = $temp_usuario;
+            }
+        }
+
         if(!strlen($temp_nombre) > 0) {
             $err_nombre = "El nombre de usuario es obligatorio";
         } else {
@@ -82,6 +97,10 @@
 
     <form action="" method="post">
         <fieldset>
+            <label>Usuario: </label>
+            <input type="text" name="usuario">
+            <?php if(isset($err_usuario)) echo $err_usuario ?>
+            <br><br>
             <label>Nombre: </label>
             <input type="text" name="nombre">
             <?php if(isset($err_nombre)) echo $err_nombre ?>
@@ -101,10 +120,11 @@
     <br><br>
 
     <?php
-    if(isset($nombre) && isset($apellidos) && isset($nacimiento)) {
+    if(isset($usuario) && isset($nombre) && isset($apellidos) && isset($nacimiento)) {
         echo strlen($apellidos);
         echo "<br>";
-        echo "$nombre $apellidos nacio el $nacimiento.";
+        echo "$nombre $apellidos nacio el $nacimiento 
+        y su nombre de usuario es $usuario.";
     }
     ?>
 </body>
